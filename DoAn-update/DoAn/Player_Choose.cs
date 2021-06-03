ac5cc853011e20a353404bd11f7849efe2ca53b8
ngocchien20042001@gmail.com <19520424@gm.uit.edu.vn>
2021-06-03 19:47:18 +0700

    chien

--- DoAn-update/DoAn/Player_Choose.cs
+++ DoAn-update/DoAn/Player_Choose.cs
@@ -31,6 +31,19 @@ namespace DoAn
             btn_Join_Room.BackgroundImageLayout = ImageLayout.Zoom;
             btn_Join_Room.Location = new Point(685, 435);
             btn_Join_Room.BackColor = Color.Transparent;
+            //Giao dien khac
+
+            lbName.Size = new Size(157, 41);
+            lbName.Location = new Point(394, 194);
+            PlayerName.Size = new Size(325, 38);
+            PlayerName.Font = new Font("Arial", 22, FontStyle.Bold);
+            PlayerName.Location = new Point(578, 197);
+
+            lbRoomName.Size = new Size(157, 41);
+            lbRoomName.Location = new Point(391, 312);
+            RoomName.Size = new Size(325, 38);
+            RoomName.Font = new Font("Arial", 22, FontStyle.Bold);
+            RoomName.Location = new Point(566, 315);
         }
 
         private void btn_Create_Room_Click(object sender, EventArgs e)
