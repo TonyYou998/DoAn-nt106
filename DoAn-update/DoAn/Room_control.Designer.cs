5afda58e301f42b7f08e9e717ee8fc9d41d3025e
Lâm Duy <nitol.duy@gmail.com>
2021-06-09 18:15:44 +0700

    UP 2

---
+++ DoAn-update/DoAn/Room_control.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace Client
+{
+    partial class Room_control
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Box_listview = new System.Windows.Forms.ListView();
+            this.RoomID = new System.Windows.Forms.ColumnHeader();
+            this.Title = new System.Windows.Forms.ColumnHeader();
+            this.Member_count = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // Box_listview
+            // 
+            this.Box_listview.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.RoomID,
+            this.Title,
+            this.Member_count});
+            this.Box_listview.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Box_listview.GridLines = true;
+            this.Box_listview.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.Box_listview.HideSelection = false;
+            this.Box_listview.Location = new System.Drawing.Point(12, 15);
+            this.Box_listview.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Box_listview.MultiSelect = false;
+            this.Box_listview.Name = "Box_listview";
+            this.Box_listview.Size = new System.Drawing.Size(1060, 550);
+            this.Box_listview.TabIndex = 0;
+            this.Box_listview.UseCompatibleStateImageBehavior = false;
+            this.Box_listview.View = System.Windows.Forms.View.Details;
+            this.Box_listview.SelectedIndexChanged += new System.EventHandler(this.Box_listview_SelectedIndexChanged);
+            // 
+            // RoomID
+            // 
+            this.RoomID.Name = "RoomID";
+            this.RoomID.Text = "Phòng";
+            this.RoomID.Width = 198;
+            // 
+            // Title
+            // 
+            this.Title.Name = "Title";
+            this.Title.Text = "Tên Phòng";
+            this.Title.Width = 356;
+            // 
+            // Member_count
+            // 
+            this.Member_count.Name = "Member_count";
+            this.Member_count.Text = "Số lượng người chơi";
+            this.Member_count.Width = 261;
+            // 
+            // Room_control
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1135, 946);
+            this.Controls.Add(this.Box_listview);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Room_control";
+            this.Text = "Room_control";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView Box_listview;
+        private System.Windows.Forms.ColumnHeader RoomID;
+        private System.Windows.Forms.ColumnHeader Title;
+        private System.Windows.Forms.ColumnHeader Member_count;
+    }
+}
