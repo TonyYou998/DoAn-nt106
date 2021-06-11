55fee80f93e854ca94147538b60719fdd1081a8f
TonyYou998 <19520931@gm.uit.edu.com>
2021-06-11 19:58:24 +0700

    push

--- DoAn-update/server/server/Main.cs
+++ DoAn-update/server/server/Main.cs
@@ -163,17 +163,17 @@ namespace Server
                 case "JoinRoom":
                     string[] Data = packet.msgcontent.Split(':');
                     //RoomID:color:list<Horse>
-                    string Color = Data[1];
+                    string Color = Data[2];
 
                     switch(Color)
                     {
-                        case "Green":
+                        case "green":
                             HC[number_room].listGreenHorse = packet.msgHorse;
                             break;
-                        case "Blue":
+                        case "blue":
                             HC[number_room].listBlueHorse = packet.msgHorse;
                             break;
-                        case "Yellow":
+                        case "yellow":
                             HC[number_room].listyellowHorse = packet.msgHorse;
                             break;
                     }
